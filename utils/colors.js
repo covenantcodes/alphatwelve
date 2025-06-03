2b06b7d94ed81da60a20ae11abb3f6f369393645
covenantcodes <covenantcodes@gmail.com>
2025-06-03 09:34:11 +0100

    Add color utility module with predefined color constants

---
+++ utils/colors.js
@@ -0,0 +1,20 @@
+const colors = {
+  primaryColor: "#078CFF",
+  secondaryColor: "#60B5FF",
+  primaryBg: "#E2FFF6",
+  white: "#ffffff",
+  background: "#FAFAFA",
+  black: "#000000",
+  red: "#DA100B",
+  green: "#22A37C",
+  gray: "##E2E8F0",
+  gray1: "#64748B",
+  gray2: "#505050",
+  gray3: "##718096",
+  gray4: "#E5E7EB",
+  blue: "#2563EB",
+  deepBlue: "#0C263A",
+  orange: "#E99B36",
+};
+
+export default colors;
