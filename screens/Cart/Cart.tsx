39697e91b72952db2279bfb7c0640a48dfc1600c
covenantcodes <covenantcodes@gmail.com>
2025-06-04 20:46:25 +0100

    feat: Refactor Cart screen to use SafeAreaView and Header component for improved layout

--- screens/Cart/Cart.tsx
+++ screens/Cart/Cart.tsx
@@ -1,21 +1,33 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { FONTFAMILY } from "../../utils/font";
+import colors from "../../utils/colors";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useNavigation } from "@react-navigation/native";
+import Header from "../../components/Header";
 
 const Cart = () => {
+  const navigation = useNavigation();
+  const handleBackAction = () => {
+    navigation.goBack();
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Cart Screen</Text>
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+      <Header
+        searchBarShow={false}
+        showActionContainer={true}
+        onActionPress={handleBackAction}
+        actionTitle="Your Cart"
+      />
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  safeArea: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#FAFAFA",
+    backgroundColor: colors.white,
   },
   text: {
     fontFamily: FONTFAMILY.medium,
